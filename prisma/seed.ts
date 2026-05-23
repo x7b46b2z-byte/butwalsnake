3e6032b2db73a3c6ec73d12e23a81acc55a7c23f
Abishekh <x7b46b2z@gmail.com>
2026-05-24 02:42:04 +0545

    chore: update seed script to use postgres

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from './generated/prisma/client/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import bcrypt from 'bcryptjs';
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
